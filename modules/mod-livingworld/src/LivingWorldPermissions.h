5918f6ac884cc842952e1c5c64768d2f8bc0c36a
mentrop92 <mentrop92@gmail.com>
2026-07-23 18:49:17 -0500

    Security: isolate LivingWorld RBAC ID range

--- modules/mod-livingworld/src/LivingWorldPermissions.h
+++ modules/mod-livingworld/src/LivingWorldPermissions.h
@@ -5,13 +5,13 @@
 
 namespace LivingWorld::Permissions
 {
-    // Module-owned RBAC range. Keep these values synchronized with
+    // High module-owned RBAC range. Keep these values synchronized with
     // data/sql/db-auth/base/livingworld_rbac.sql.
-    inline constexpr std::uint32_t View = 1900;
-    inline constexpr std::uint32_t ProfileInspect = 1901;
-    inline constexpr std::uint32_t RuntimeControl = 1902;
-    inline constexpr std::uint32_t PopulationControl = 1903;
-    inline constexpr std::uint32_t DestructiveSeed = 1904;
+    inline constexpr std::uint32_t View = 19000;
+    inline constexpr std::uint32_t ProfileInspect = 19001;
+    inline constexpr std::uint32_t RuntimeControl = 19002;
+    inline constexpr std::uint32_t PopulationControl = 19003;
+    inline constexpr std::uint32_t DestructiveSeed = 19004;
 }
 
 #endif
