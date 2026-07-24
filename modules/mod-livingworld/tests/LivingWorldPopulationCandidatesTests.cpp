7c0d989cc0c8c25a4cd4f75746594175da9748ff
mentrop92 <mentrop92@gmail.com>
2026-07-24 00:28:05 -0500

    Test dynamic logout readiness ranking

--- modules/mod-livingworld/tests/LivingWorldPopulationCandidatesTests.cpp
+++ modules/mod-livingworld/tests/LivingWorldPopulationCandidatesTests.cpp
@@ -9,13 +9,14 @@ using namespace LivingWorld;
 int main()
 {
     std::vector<PopulationCandidate> candidates = {
-        { 1, false, true, false, false, false, false, false, false, false, 0, 0 },
-        { 2, false, false, false, false, false, true, false, false, false, 30, 0 },
-        { 3, true, false, true, false, false, false, false, false, false, 0, 45 },
-        { 4, true, false, true, true, false, false, false, false, false, 0, 120 },
-        { 5, true, false, true, false, true, true, true, true, false, 0, 180 },
-        { 6, true, false, false, false, false, false, false, false, false, 0, 0 },
-        { 7, true, false, true, false, false, false, false, false, true, 0, 300 }
+        { 1, false, true, false, false, false, false, false, false, false, 0, 0, 0 },
+        { 2, false, false, false, false, false, true, false, false, false, 30, 0, 0 },
+        { 3, true, false, true, false, false, false, false, false, false, 0, 45, 80 },
+        { 4, true, false, true, true, false, false, false, false, false, 0, 120, 100 },
+        { 5, true, false, true, false, true, true, true, true, false, 0, 180, 90 },
+        { 6, true, false, false, false, false, false, false, false, false, 0, 0, 20 },
+        { 7, true, false, true, false, false, false, false, false, true, 0, 300, 100 },
+        { 8, true, false, false, false, false, false, false, false, false, 0, 0, 95 }
     };
 
     auto login = PopulationCandidateSelector::RankLoginCandidates(candidates, 2);
@@ -24,12 +25,13 @@ int main()
     assert(login[1].characterGuid == 2);
 
     auto logout = PopulationCandidateSelector::RankLogoutCandidates(candidates, 10, false);
-    assert(logout.size() == 2);
+    assert(logout.size() == 3);
     assert(logout[0].characterGuid == 3);
-    assert(logout[1].characterGuid == 6);
+    assert(logout[1].characterGuid == 8);
+    assert(logout[2].characterGuid == 6);
 
     auto emergency = PopulationCandidateSelector::RankLogoutCandidates(candidates, 10, true);
-    assert(emergency.size() == 5);
+    assert(emergency.size() == 6);
     assert(emergency[0].characterGuid == 3);
     assert(emergency.back().characterGuid == 7);
 
