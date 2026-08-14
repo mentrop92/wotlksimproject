810faf46619ad3c368e9b7dd9a5b0f1c480d1fc4
mentrop92 <mentrop92@gmail.com>
2026-08-14 06:43:46 -0400

    Extend bounded observation serialization interface

--- modules/mod-livingworld/src/LivingWorldSimulationObservationSerialization.h
+++ modules/mod-livingworld/src/LivingWorldSimulationObservationSerialization.h
@@ -13,8 +13,15 @@ class SimulationObservationSerializer
 public:
     static constexpr std::size_t MaxSerializedBytes = 1024;
     static constexpr std::size_t MaxCorrelationIdBytes = 96;
+
     static std::optional<std::string> SerializeSummary(SimulationObservationSummary const& value);
     static std::optional<SimulationObservationSummary> DeserializeSummary(std::string const& payload);
+
+    static std::optional<std::string> SerializeDiagnostic(SimulationCorrelationDiagnostic const& value);
+    static std::optional<SimulationCorrelationDiagnostic> DeserializeDiagnostic(std::string const& payload);
+
+private:
+    static bool IsSafeCorrelationId(std::string const& value);
 };
 }
 
